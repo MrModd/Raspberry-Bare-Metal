439ec1766213542d0d01f36caf1f8280ee038fee
Federico Cosentino <fed.cosentino@gmail.com>
2014-11-28 23:48:49 +0100

    Blinking LED

---
+++ 04-blink/common.h
@@ -0,0 +1,39 @@
+/*
+ * Raspberry Bare Metal
+ * Copyright (C) 2014  Federico "MrModd" Cosentino (http://mrmodd.it/)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef RASPBERRY_H
+#error You should not include sub-header files
+#endif
+
+/* Define the entry point function symbol that may be used by some functions
+ * that include raspberry.h header file (such as init.c) */
+extern void entry(void);
+
+/* inline tells to the compiler to optimize, when possible, this function
+ * replacing the function call with the function itself */ 
+static inline void loop_delay(unsigned long d)
+{
+	volatile unsigned long c;
+	/* Making the variable volatile we force the compiler to read
+	 * its content from main memory each time, discarding eventual
+	 * cache copies */
+	while (d-- > 0)
+		c += d;
+		/* This instruction is just to prevent to leave an empty while.
+		 * Compiler optimization could remove the loop */
+}
---
+++ 04-blink/entry.c
@@ -0,0 +1,41 @@
+/*
+ * Raspberry Bare Metal
+ * Copyright (C) 2014  Federico "MrModd" Cosentino (http://mrmodd.it/)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "raspberry.h"
+
+volatile u32 *gpio = (unsigned int *) GPIO_BASE;
+
+void entry(void)
+{
+	/* Init GPIO 16 (wired to LED ACT) as Output */
+	gpio[GPFSEL1/4] |= (1<<18);
+	
+	/* Alternativelly: */
+	//gpio[GPFSEL1/4] &= ~((1<<20)|(1<<19)|(1<<18));
+	//gpio[GPFSEL1/4] |= ((0<<20)|(0<<19)|(1<<18));
+	
+	/* Set bit related to GPIO 16 low */
+	//gpio[GPCLR0/4] |= (1<<16);
+	
+	for(;;) {
+		gpio[GPCLR0/4] |= (1<<16);
+		loop_delay(10000000);
+		gpio[GPSET0/4] |= (1<<16);
+		loop_delay(10000000);
+	}
+}
