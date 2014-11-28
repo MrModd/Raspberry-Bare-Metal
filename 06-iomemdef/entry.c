bbcad7bcfb9d24eb544f4b4a198f0368ec1e02db
Federico Cosentino <fed.cosentino@gmail.com>
2014-11-28 23:50:47 +0100

    Addresses definitions as constants, less memory access

---
+++ 06-iomemdef/entry.c
@@ -0,0 +1,50 @@
+/*
+ * Raspberry Bare Metal
+ * Copyright (C) 2014  Federico "MrModd" Cosentino (http://mrmodd.it/)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "raspberry.h"
+
+void entry(void)
+{
+	for(;;) {
+		
+		LED_TOGGLE
+		loop_delay(10000000);
+		
+		/* Or */
+		/*
+		GPIO0_TOGGLE_BIT(16)
+		loop_delay(10000000);
+		*/
+		
+		/* Or */
+		/*
+		GPIO0_SET_LOW(16)
+		loop_delay(10000000);
+		GPIO0_SET_HIGH(16)
+		loop_delay(10000000);
+		*/
+		
+		/* Or */
+		/*
+		iomem(GPCLR0) |= (1<<16);
+		loop_delay(10000000);
+		iomem(GPSET0) |= (1<<16);
+		loop_delay(10000000);
+		*/
+	}
+}
---
+++ 06-iomemdef/init.c
@@ -0,0 +1,73 @@
+/*
+ * Raspberry Bare Metal
+ * Copyright (C) 2014  Federico "MrModd" Cosentino (http://mrmodd.it/)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "raspberry.h"
+
+/* Init to 0 section .bss, where static variables that
+ * must be initialized to 0 are located */
+void init_bss()
+{
+	/* _bss_start and _bss_end symbols are defined during
+	 * linking process in file sert.lds */
+	extern u32 _bss_start, _bss_end;
+	u32 *p;
+	
+	for (p = &_bss_start; p < &_bss_end; p++)
+		*p = 0UL;
+	
+	/* Why we use &_bss_start and &_bss_end?
+	 * We must set to 0 all memory cells between _bss_start and
+	 * _bss_end, so we must iterate on the address of these
+	 * variables:
+	 *                  ___________
+	 * _bss_end        |           |
+	 *                 |           |
+	 *                 |           |
+	 *                 |           |
+	 * _bss_start      |           |
+	 *                 |___________|
+	 * 
+	 * these symbols doesn't point to the memory area to erase,
+	 * they ARE the memory addresses to erase. So we put p equals
+	 * to the address of _bss_start and we iterate until _bss_end */
+}
+
+void init_gpio()
+{
+	/* Init GPIO 16 (wired to LED ACT) as Output */
+	
+	//iomem(GPFSEL1) |= (1<<18);
+	
+	/* Or */
+	//gpio[GPFSEL1/4] &= ~((1<<20)|(1<<19)|(1<<18));
+	//gpio[GPFSEL1/4] |= ((0<<20)|(0<<19)|(1<<18));
+	
+	/* Or */
+	
+	GPIO1_SET_OUTPUT(GPIO_LED_SEL_OFFSET)
+}
+
+/* Init memory periferals and then jump to entry() */
+void _init()
+{
+	init_bss();
+	init_gpio();
+	
+	/* Jump to the entry point function that run user defined code */
+	entry();
+}
